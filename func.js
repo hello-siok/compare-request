bcd383dc3851d06534bf2af87a4f0fc40dd8f124
hello-siok <gohsw314@gmail.com>
2024-04-18 15:21:12 +0800

    add new logic to show missing attributes

--- func.js
+++ func.js
@@ -1,7 +1,8 @@
 var parsedJson = [];
 var yesNoDict = {};
 var requestInputCount = 2;
-
+var requiredAttributeId = [];
+var missingAttributesArray = [];
 
 function readInputAsJSON() {
     // reset global variables everytime clicking
@@ -20,14 +21,22 @@ function readInputAsJSON() {
             parsedJson.push(JSON.parse(jsonInput[i].value));
             validRequestCount += 1;
         } catch (error) {
-            console.log ("Invalid JSON input");
+            console.log("Invalid JSON input");
         }
     }
 
     
     // call next functions
+    // get required attribute array
+    fetchRequiredAttribute();
+
+    // render correct table header
     renderTableHead(validRequestCount);
+
+    // clear old results before add new result
     clearTableResults();
+
+    // compare starts
     compareJson();
     displayResult();
     //console.log(parsedJson);
@@ -37,8 +46,9 @@ function readInputAsJSON() {
  * we iterate over request JSON
  * transform request JSON to allAttribute
  * from allAttribute, check the present/absent of attribute
+ * also check if missing important attribute
  * update diciotnary yesNoDict
- * yesNoDict = {“key”: {parent, attribute, yesNoSequence= [y, n], valueSequence = [] } }
+ * yesNoDict = {“key”: {parent, attribute, yesNoSequence= [y, n], valueSequence = [] }}
 **/
 function compareJson(){
     // iterate over dict of parsedJson
@@ -61,10 +71,17 @@ function compareJson(){
         //- iterate over allAttribute -> update yesNoDict
         //-- if it is first request
         if (i == 0){
+            //-- reset the array to put important attributes that are missing
+            missingAttributesArray = [[]];
 
             for(let element of jason){ //iterate over allAttributes
 
-                //-- if there is attribute present, add to yesNoDict
+                //-- missing attribute check, add to missingAttributesArray
+                if(element["location"] == null && element["Required"] == 1){
+                    missingAttributesArray[0].push(element["id"]);
+                }
+
+                //-- present attribute check, add to yesNoDict
                 if(element["location"] !== null){
                     let key = element["id"];
                     let value = {};
@@ -80,10 +97,17 @@ function compareJson(){
 
         //-- second request onwards
         } else {
+            //add a new array to missingAttributesArray
+            missingAttributesArray.push([]);
 
             for(let element of jason){
                 let key = element["id"];
                 let attributeValue = element["location"];
+
+                //-- missing attribute check, add to missingAttributesArray
+                if(element["location"] == null && element["Required"] == 1){
+                    missingAttributesArray[i].push(element["id"]);
+                }
 
                 //--- if attribute is present @request, key present @yesNoDict : add to existing key
                 if (yesNoDict.hasOwnProperty(key) && attributeValue !== null ){
@@ -116,12 +140,10 @@ function compareJson(){
         }
     }
 
-    console.log(yesNoDict);
+    console.log(missingAttributesArray);
 }
 
 function displayResult(){
-    //console.log("displayResult got triggered!");
-    
     const table = document.getElementById("result"); 
 
     //iterate over yesNoDict
@@ -154,8 +176,26 @@ function displayResult(){
         rowHTML += `</tr>`;
         table.innerHTML += rowHTML;
     }
+
+    // display missing attributes
+    const missingAttributeTable = document.getElementById("missingAttribute-result"); 
+    var row2HTML = `<tr><td>Completeness Score</td>`;
+    var row3HTML = `<tr><td>List of Missing Attributes</td>`;
+
+    for(let i = 0; i < missingAttributesArray.length; i++){
+        let completenessScore = 1 - (missingAttributesArray[i].length/requiredAttributeId.length);
+
+        row2HTML += `<td>${completenessScore * 100}%</td>`;
+        row3HTML += `<td>${missingAttributesArray[i]}%</td>`;
+    }
+
+    row2HTML += `</tr>`;
+    row3HTML += `</tr>`;
+    missingAttributeTable.innerHTML += row2HTML;
+    missingAttributeTable.innerHTML += row3HTML;
 }
 
+// add a textarea when users click "+" button 
 function addRequest(){
     const userInput = document.getElementById("user-input"); 
     requestInputCount += 1;
@@ -178,27 +218,63 @@ function addRequest(){
 }
 
 function renderTableHead(validRequestCount){
-    // update the table column
-    const th = document.getElementById("table-head"); 
-
-    let thHTML = `<th scope="col">Object</th>
+    // prepare COMPARE table column
+    const th_compareRequest = document.getElementById("table-head-compare-request"); 
+    let thHTML_compareRequest = `<th scope="col">Object</th>
     <th scope="col">Attribute</th>`;
+
+    // prepare missing attribute table column
+    const th_missingAttribute = document.getElementById("table-head-missing-attribute"); 
+    let thHTML_missingAttribute = `<th scope="col"> </th>`;
 
     //retrieve input name
     let requestName = document.querySelectorAll("#requestName");
 
+    // 'present in' column
     for(let i = 0; i < validRequestCount; i++){
-        thHTML += `<th scope="col">Present in ${requestName[i].value}? (Y/N)</th>`; 
+        thHTML_compareRequest += `<th scope="col">Present in ${requestName[i].value}? (Y/N)</th>`; 
+        thHTML_missingAttribute += `<th scope="col">${requestName[i].value}</th>`;
     }
 
+    // 'value in' column
     for(let i = 0; i < validRequestCount; i++){
-        thHTML += `<th scope="col">Value in ${requestName[i].value}</th>`; 
+        thHTML_compareRequest += `<th scope="col">Value in ${requestName[i].value}</th>`; 
     }
 
-    th.innerHTML = thHTML;
+    th_compareRequest.innerHTML = thHTML_compareRequest;
+    th_missingAttribute.innerHTML = thHTML_missingAttribute;
 }
 
 function clearTableResults(){
     const result = document.getElementById("result"); 
     result.innerHTML = "";
+
+    const missingAttributeResult = document.getElementById("missingAttribute-result"); 
+    missingAttributeResult.innerHTML = "";
+}
+
+/**
+ * logic to retrieve missing Attributes 
+ * 1. get an array of required attributes => func fetchRequiredAttribute()
+ * 2. check if the attributes present in attributes array, requiredAttributeId
+ * 3. if not present, add to an array, yesNoDict = {parent, attribute .. missingAttributes = []} }
+ * render the results into table
+ * score = Present array length / required array length
+ * */
+function fetchRequiredAttribute(){
+    if (requiredAttributeId.length > 0){
+        return requiredAttributeId;
+    }
+
+    let allAttributes = getJason({}, {});
+
+    allAttributes.forEach(function(attribute) {
+        if(attribute["Required"] == 1){
+            requiredAttributeId.push(attribute["id"]);
+        }
+    });
+
+    //console.log(requiredAttributeId);
+    return requiredAttributeId;
+
 }
